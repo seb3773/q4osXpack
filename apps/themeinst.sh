03a377359698c7c28d4ff12eae73b0aec062bb36
seb3773 <99963207+seb3773@users.noreply.github.com>
2024-03-21 12:40:47 +0100

    Add files via upload

--- apps/themeinst.sh
+++ apps/themeinst.sh
@@ -1,67 +1,46 @@
 #!/bin/bash
-if [ -t 0 ]; then interm=1; else interm=0; fi
-if [ $# -eq 0 ]; then
-if  [ $interm -eq 1 ]; then
-echo "Usage:";echo "- Apply themepack:      $0 <xxxx.themepack>";echo "- Install association:  $0 install"
-fi; exit 1; fi
-USER_HOME=$(eval echo ~${SUDO_USER})
-if [ "$interm" -eq 1 ] && [ "$1" = "install" ]; then
-echo "installing...";scriptfolder=$(dirname "$(readlink -f "$0")")
-desktopfilecontent="[Desktop Entry]
-Exec[\$e]=$scriptfolder/themeinst.sh
-MimeType=application/ms_themepack
+n="themepack";if [ -t 0 ];then term=1;else term=0;fi
+if [ $# -eq 0 ];then if [ $term -eq 1 ];then
+echo "Usage:";echo "- Apply $n :    $0 <xxxx.themepack>";echo "- Install association: $0 install"
+fi;exit 1;fi;USH=$(eval echo ~${SUDO_USER})
+if [ "$term" -eq 1 ] && [ "$1" = "install" ];then echo "installing...";scfold=$(dirname "$(readlink -f "$0")")
+dskf="[Desktop Entry]
+Exec[\$e]=$scfold/themeinst.sh
+MimeType=application/ms_$n
 Icon=preferences-desktop-theme
-Name=MS themepack installer
+Name=MS $n installer
 Terminal=false
 Type=Application
 X-TDE-InitialPreference=2
-";desktopfilepath="$HOME/.trinity/share/applnk/.hidden/themeinst.sh.desktop"
-sudo mkdir -p "$HOME/.trinity/share/applnk/.hidden"
-echo "$desktopfilecontent" | sudo tee "$desktopfilepath" > /dev/null 2>&1
-desktopfilecontent="[Desktop Entry]
-Exec[\$e]=$scriptfolder/themeinst.sh
-MimeType=application/ms_deskthemepack
-Icon=preferences-desktop-theme
-Name=MS themepack installer
-Terminal=false
-Type=Application
-X-TDE-InitialPreference=2
-";desktopfilepath="$HOME/.trinity/share/applnk/.hidden/themeinst.sh-2.desktop"
-sudo mkdir -p "$HOME/.trinity/share/applnk/.hidden"
-echo "$desktopfilecontent" | sudo tee "$desktopfilepath" > /dev/null 2>&1
-mdesktopfilecontent="[Desktop Entry]
-Comment=Microsoft themepack
+";deskfp="$HOME/.trinity/share/applnk/.hidden/themeinst.sh.desktop"
+mkdir -p "$HOME/.trinity/share/applnk/.hidden";echo "$dskf" | tee "$deskfp" > /dev/null 2>&1
+deskfp2="$HOME/.trinity/share/applnk/.hidden/themeinst.sh-2.desktop"
+\cp "$deskfp" "$deskfp2"
+sed -i 's/^MimeType=.*/MimeType=application\/ms_deskthemepack/' "$HOME/.trinity/share/applnk/.hidden/themeinst.sh-2.desktop"
+mdskf="[Desktop Entry]
+Comment=Microsoft $n
 Hidden=false
 Icon=preferences-desktop-wallpaper
-MimeType=application/ms_themepack
-Patterns=*.themepack
+MimeType=application/ms_$n
+Patterns=*.$n
 Type=MimeType
-";mdesktopfilepath="$HOME/.trinity/share/mimelnk/application/ms_themepack.desktop"
-echo "$mdesktopfilecontent" | sudo tee "$mdesktopfilepath" > /dev/null 2>&1
-mdesktopfilecontent="[Desktop Entry]
-Comment=Microsoft deskthemepack
-Hidden=false
-Icon=preferences-desktop-wallpaper
-MimeType=application/ms_deskthemepack
-Patterns=*.deskthemepack
-Type=MimeType
-";mdesktopfilepath="$HOME/.trinity/share/mimelnk/application/ms_deskthemepack.desktop"
-echo "$mdesktopfilecontent" | sudo tee "$mdesktopfilepath" > /dev/null 2>&1
-echo "Done."; exit; fi
-filename=$(basename "$1")
-if [[ $filename == *.themepack ]]; then filename_no_extension="${filename%.themepack}"
-elif [[ $filename == *.deskthemepack ]]; then filename_no_extension="${filename%.deskthemepack}"
-else echo "Extension must be .themepack or .deskthemepack"; exit; fi
-filename=$filename_no_extension
-wallpapers_dir="$TDEHOME/share/wallpapers/$filename"
-mkdir -p "$wallpapers_dir"
-7z x "$1" -o"$wallpapers_dir" -y > /dev/null 2>&1
-rm -f "$TDEHOME/share/wallpapers/$filename/"*.theme
-kwriteconfig --file $USER_HOME/.trinity/share/config/kdesktoprc --group Desktop0 --key WallpaperList "$TDEHOME/share/wallpapers/$filename/DesktopBackground/"
-kwriteconfig --file $USER_HOME/.trinity/share/config/kdesktoprc --group Desktop0 --key MultiWallpaperMode Random
-kwriteconfig --file $USER_HOME/.trinity/share/config/kdesktoprc --group Desktop0 --key CrossFadeBg true
-kwriteconfig --file $USER_HOME/.trinity/share/config/kdesktoprc --group Desktop0 --key ChangeInterval 10
+";mdeskfp="$HOME/.trinity/share/mimelnk/application/ms_themepack.desktop"
+echo "$mdskf" | tee "$mdeskfp" > /dev/null 2>&1
+mdeskfp2="$HOME/.trinity/share/mimelnk/application/ms_deskthemepack.desktop"
+\cp "$mdeskfp" "$mdeskfp2"
+sed -i 's/^MimeType=.*/MimeType=application\/ms_deskthemepack/' "$mdeskfp2"
+sed -i 's/^Patterns=.*/Patterns=*.deskthemepack/' "$mdeskfp2"
+echo "Done.";exit;fi
+fn=$(basename "$1");if [[ $fn == *.themepack ]];then fn_noext="${fn%.themepack}"
+elif [[ $fn == *.deskthemepack ]];then fn_noext="${fn%.deskthemepack}"
+else echo "Extension must be .$n or .desk$n";exit;fi
+fn=$fn_noext;wpdir="$TDEHOME/share/wallpapers/$fn";mkdir -p "$wpdir"
+7z x "$1" -o"$wpdir" -y > /dev/null 2>&1
+rm -f "$TDEHOME/share/wallpapers/$fn/"*.theme
+kwriteconfig --file "$USH/.trinity/share/config/kdesktoprc" --group Desktop0 --key WallpaperList "$TDEHOME/share/wallpapers/$fn/DesktopBackground/"
+kwriteconfig --file "$USH/.trinity/share/config/kdesktoprc" --group Desktop0 --key MultiWallpaperMode Random
+kwriteconfig --file "$USH/.trinity/share/config/kdesktoprc" --group Desktop0 --key CrossFadeBg true
+kwriteconfig --file "$USH/.trinity/share/config/kdesktoprc" --group Desktop0 --key ChangeInterval 10
 dcop kdesktop KBackgroundIface configure
-if  [ $interm -eq 1 ]; then echo "Themepack $filename installed."
-else dcop knotify Notify notify "Themepack installation" "knotify" "Themepack $filename Applied." "" "" 16 2
-fi
+if  [ $term -eq 1 ];then echo "$n $fn installed."
+else dcop knotify Notify notify "$n installation" "knotify" "$n $fn applied." "" "" 16 2;fi
