baf1fd0ea6e8c697f013acde0e53c76faceb57da
seb3773 <99963207+seb3773@users.noreply.github.com>
2024-04-09 16:34:17 +0200

    Delete perfs directory

--- perfs/repository.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "  \e[35m░▒▓█\033[0m add repository..."
-wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
-echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-release.list
